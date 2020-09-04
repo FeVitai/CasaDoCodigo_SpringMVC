231dc365f5a747568159e9788c555565f0b57018
FeVitai <felipevitai93@gmail.com>
2020-09-04 19:03:01 -0300

    new stuff

--- src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,12 +27,17 @@ public class CarrinhoComprasController {
 
 	@RequestMapping("/add")
 	public ModelAndView add(Integer produtoId, TipoPreco tipoPreco) {
-		ModelAndView modelAndView = new ModelAndView("redirect:/produtos");
+		ModelAndView modelAndView = new ModelAndView("redirect:/carrinho");
 		CarrinhoItem carrinhoItem = criaItem(produtoId, tipoPreco);
 		
 		carrinho.add(carrinhoItem);
 		
 		return modelAndView;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView itens () {
+		return new ModelAndView("carrinho/itens");
 	}
 
 	private CarrinhoItem criaItem(Integer produtoId, TipoPreco tipoPreco) {
--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,6 @@
 package br.com.casadocodigo.loja.models;
 
+import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.List;
 
@@ -114,6 +115,11 @@ public class Produto {
 		if (id != other.id)
 			return false;
 		return true;
+	}
+
+	public BigDecimal precoPara(TipoPreco tipoPreco) {
+		return precos.stream().filter(preco -> preco.getTipo().equals(tipoPreco))
+				.findFirst().get().getValor();
 	}
 	
 }
