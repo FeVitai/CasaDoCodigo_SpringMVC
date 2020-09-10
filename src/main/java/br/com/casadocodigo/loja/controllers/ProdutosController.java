c0b1d848b06229ebef3419ee4fa7e934b2fe8f68
FeVitai <felipevitai93@gmail.com>
2020-09-10 06:47:32 -0300

    new stuff

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -83,6 +84,12 @@ public class ProdutosController {
 		modelAndView.addObject("produto", produto);
 		
 		return modelAndView;
+	}
+	
+	@RequestMapping("/{id}")
+	@ResponseBody
+	public Produto detalheJson(@PathVariable("id") Integer id) {
+		return produtoDao.find(id);
 	}
 	
 }
